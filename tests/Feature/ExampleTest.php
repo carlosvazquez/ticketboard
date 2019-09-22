37fdbc16d58b59d36d3126942c91072259ec3351
Carlos Vazquez <carlosvazquezmurillo@gmail.com>
2019-09-22 13:23:05 -0500

    Refacto test example

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,19 +2,20 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    use WithFaker, RefreshDatabase;
+
+    /** @test */
+    public function a_basic_test_case()
     {
         $response = $this->get('/');
+        // $get = $this->get('/');
+        // $post = $this->post('/');
 
         $response->assertStatus(200);
     }
